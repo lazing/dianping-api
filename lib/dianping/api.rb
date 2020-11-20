0cc132e22c58c716c210edb5303d16be8ade0c40
Ryan Wong <lazing@gmail.com>
2020-11-20 11:08:35 +0800

    init submit

---
+++ lib/dianping/api.rb
@@ -0,0 +1,8 @@
+require "dianping/api/version"
+
+module Dianping
+  module Api
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/dianping/api/version.rb
@@ -0,0 +1,5 @@
+module Dianping
+  module Api
+    VERSION = "0.1.0"
+  end
+end
